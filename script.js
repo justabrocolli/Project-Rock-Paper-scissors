bc4d19faf86ecdc21a1fe4f52e9394791db9d5bb
justabrocolli <eyal829@gmail.com>
2024-11-14 18:49:28 +0200

    finished pc's choice now to generates a random choise between rock paper and scissors

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("We are LIVE!")
+
+function getComputerChoice(){
+    let choice = Math.floor(1+Math.random()*3);
+    let answer;
+    switch (choice){
+        case 1:
+            answer = "rock";
+        break;
+        case 2:
+            answer = "paper";
+        break;
+        case 3:
+            answer = "scissors";
+        break;
+    }
+    return answer;
+}
+
+//let userChoice = prompt("Choose: Rock | Paper | Scissors");
+//userChoice.toLocaleLowerCase;
+
+
+console.log(getComputerChoice());
