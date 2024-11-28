0caf42560ef45fe02b5e8e4e872e0cefe34b6a43
justabrocolli <eyal829@gmail.com>
2024-11-28 22:21:16 +0200

    adding styles and ui for the player

--- script.js
+++ script.js
@@ -1,82 +1,116 @@
 console.log("We are LIVE!")
+let userChoice = document.querySelectorAll(".btn");
+let humanScore = 0;
+let pcScore = 0;
+let round = 1;
+console.log(userChoice);
+
+
 function getComputerChoice(){
     let choice = Math.floor(1+Math.random()*3);
     let answer;
     switch (choice){
         case 1:
-            answer = "rock";
+            answer = "Rock";
         break;
         case 2:
-            answer = "paper";
+            answer = "Paper";
         break;
         case 3:
-            answer = "scissors";
+            answer = "Scissors";
         break;
     }
     return answer;
 }
-function getUserChoice(){
-    let userChoice = prompt("Choose: Rock | Paper | Scissors");
-    userChoice.toLocaleLowerCase;
-    switch(userChoice){
-        case 'rock':
-            return userChoice;
-        break;
-        case 'paper':
-            return userChoice;
-        break;
-        case 'scissors':
-            return userChoice;
-        break;
-    }
-    return alert("invalid option please try again...") + getUserChoice();
+
+function getUserChoice(event){
+    
+    let userChoice =  event.target.textContent;
+    console.log(userChoice);
+   // userChoice.toLocaleLowerCase;
+   switch (userChoice){
+    case 1:
+        answer = "Rock";
+    break;
+    case 2:
+        answer = "Paper";
+    break;
+    case 3:
+        answer = "Scissors";
+    break;
+}
+let pc = getComputerChoice();
+   console.log(match(userChoice,pc));
 }
 
-function match(player , PC){
-    switch(player){
-        case 'rock':
-            if(PC == 'paper'){
+function match(player , pc){
+let _player = player;
+let _Pc = pc;
+display(_player, pc);
+    let scoreText = document.querySelector(".score")
+    switch(_player){
+        case 'Rock':
+            if(_Pc == 'Paper'){
                 pcScore++;
-                return alert(`YOU LOST...`);
+                return alert(`YOU LOST...`) , scoreText.textContent = `Won: ${humanScore} lost: ${pcScore}`;
             } 
-            else if(PC == 'scissors'){
+            else if(_Pc == 'Scissors'){
                 humanScore++;
-                return alert(`YOU WON!`);
+                return alert(`YOU WON!`),  scoreText.textContent = `Won: ${humanScore} lost: ${pcScore}`;
             }
-            else
-                return alert(`A TIE`);
-        break;
-        case 'paper':
-            if(PC == 'scissors'){
+            else{
+                return alert(`A TIE`), scoreText.textContent = `Won: ${humanScore} lost: ${pcScore}`;
+            }
+        case 'Paper':
+            if(_Pc == 'Scissors'){
                 pcScore++;
-                return alert(`YOU LOST...`);
+                return alert(`YOU LOST...`),  scoreText.textContent = `Won: ${humanScore} lost: ${pcScore}`;
             } 
-            else if(PC == 'rock'){
+            else if(_Pc == 'Rock'){
                 humanScore++;
-                return alert(`YOU WON!`);
+                return alert(`YOU WON!`), scoreText.textContent = `Won: ${humanScore} lost: ${pcScore}`;
             }
-            else
-                return alert(`A TIE`);
-        break;
-        case 'scissors':
-            if(PC == 'rock'){
+            else{
+                return alert(`A TIE`), scoreText.textContent = `Won: ${humanScore} lost: ${pcScore}`;
+            }
+
+        case 'Scissors':
+            if(_Pc == 'Rock'){
                 pcScore++;
-                return alert(`YOU LOST...`)
+                return alert(`YOU LOST...`), scoreText.textContent = `Won: ${humanScore} lost: ${pcScore}`
             } 
-            else if(PC == 'paper'){
+            else if(_Pc == 'Paper'){
                 humanScore++;
-                return alert(`YOU WON!`);
+                return alert(`YOU WON!`),  scoreText.textContent = `Won: ${humanScore} lost: ${pcScore}`;
+               
             }
-            else
-                return alert(`A TIE`);
-        break;
+            else{
+                return alert(`A TIE`),  scoreText.textContent = `Won: ${humanScore} lost: ${pcScore}`;
+            }
     }
     return 0;
 }
+function display(player , pc){
+    let _p1 = document.querySelector("#p1");
+    let _pc = document.querySelector("#pc");
 
-let humanScore = 0;
-let pcScore = 0;
-let round = 1;
+    
+    if(player == "Rock")
+    _p1.src = "imgs/rck.png";
+    if(player == "Paper")
+        _p1.src = "imgs/paper.png";
+    if(player == "Scissors")
+        _p1.src = "imgs/scissers.png";
+
+    if(pc == "Rock")
+        _pc.src = "imgs/rck.png";
+    if(pc == "Paper")
+        _pc.src = "imgs/paper.png";
+    if(pc == "Scissors")
+        _pc.src = "imgs/scissers.png";
+}
+
+/*
 while((humanScore < 5) && (pcScore < 5)){
     console.log(`Round ${round}`);
     const player = getUserChoice();
@@ -88,4 +122,8 @@ while((humanScore < 5) && (pcScore < 5)){
 }
 if(humanScore == 5){alert("Good Job You Won!")}
 if(pcScore == 5){alert("LOSER...Better luck next time NERD")}
+*/
+userChoice.forEach(button => {
+    button.addEventListener("click", getUserChoice);
+});
 
